b18119377dc4584d080e3b1a164a05702ad4f132
lastlink <theefunk@gmail.com>
2019-06-13 14:24:55 -0400

    csv file parsing

--- Firefly.Import/Program.cs
+++ Firefly.Import/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace Firefly.Import
@@ -32,18 +34,28 @@ namespace Firefly.Import
                 Console.WriteLine($"Output file is \" {outputfile.Value()}!");
                 Console.WriteLine($"Bank file is \" {bank.Value()}!");
 
+                // var column1 = new List<string>();
+                // var column2 = new List<string>();
+                using (var rd = new StreamReader(inputfile.Value()))
+                {
+                    using (var csv_file = new StreamWriter(outputfile.Value()))
+                    {
+                        while (!rd.EndOfStream)
+                        {
 
-                if (args.Length > 0)
-                {
-                    Console.WriteLine($"Hello {args[0]}!");
-                    Console.WriteLine($"Hello {args[1]}!");
+                            var splits = rd.ReadLine().Split(',');
+                            Console.WriteLine(string.Join(",", splits));
+                            // column1.Add(splits[0]);
+
+                            // csv_file.WriteLine(string.Join(",", splits));
+                            // column2.Add(splits[1]);
+                        }
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Hello!");
-                }
-                Console.WriteLine(string.Join(",", args));
-                Console.WriteLine("Hello World!");
+                // Console.WriteLine("Column 1:");
+                // foreach (var element in column1)
+                //     Console.WriteLine(element);
+                    
             });
 
             app.Execute(args);
